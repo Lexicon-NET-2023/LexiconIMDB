7dfca6ee879098c665df1b823704349b6eaa9f8f
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-10-31 13:39:41 +0100

    Alter Movie, Add Seed

--- LexiconIMDB/Models/Entities/Movie.cs
+++ LexiconIMDB/Models/Entities/Movie.cs
@@ -5,7 +5,12 @@ namespace LexiconIMDB.Models.Entities
     public class Movie
     {
         public int Id { get; set; }
+
+        [StringLength(20)]
         public string Title { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Release date")]
         public DateTime ReleaseDate { get; set; }
         public Genre Genre { get; set; }
         public float Rating { get; set; }
