b4a0259f20ebf6e9e479bb930974e9861c05e276
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-10-31 11:34:47 +0100

    Add Entites

---
+++ LexiconIMDB/Models/Entities/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LexiconIMDB.Models.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public DateTime ReleaseDate { get; set; }
+        public Genre Genre { get; set; }
+        public float Rating { get; set; }
+    }
+}
