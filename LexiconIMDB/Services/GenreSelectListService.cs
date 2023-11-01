c802e366a631b51b3740f4a13dd65a460bb6d5a2
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-01 11:34:17 +0100

    Refactor Service

--- LexiconIMDB/Services/GenreSelectListService.cs
+++ LexiconIMDB/Services/GenreSelectListService.cs
@@ -1,4 +1,5 @@
 ﻿using LexiconIMDB.Data;
+using LexiconIMDB.Models.Entities;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
@@ -6,23 +7,23 @@ namespace LexiconIMDB.Services
 {
     public class GenreSelectListService : IGenreSelectListService
     {
-        private readonly LexiconIMDBContext _context;
+        //private readonly LexiconIMDBContext _context;
 
-        public GenreSelectListService(LexiconIMDBContext context)
-        {
-            _context = context;
-        }
+        //public GenreSelectListService(LexiconIMDBContext context)
+        //{
+        //    _context = context;
+        //}
 
-        public async Task<IEnumerable<SelectListItem>> GetGenresAsync()
+        public IEnumerable<SelectListItem> GetGenres(IEnumerable<Movie> movies)
         {
-            return await _context.Movie.Select(m => m.Genre)
+            return  movies.Select(m => m.Genre)
                                .Distinct()
                                .Select(g => new SelectListItem
                                {
                                    Text = g.ToString(),
                                    Value = g.ToString()
                                })
-                               .ToListAsync();
+                               .ToList();
         }
     }
 }
--- LexiconIMDB/Services/IGenreSelectListService.cs
+++ LexiconIMDB/Services/IGenreSelectListService.cs
@@ -1,9 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using LexiconIMDB.Models.Entities;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace LexiconIMDB.Services
 {
     public interface IGenreSelectListService
     {
-        Task<IEnumerable<SelectListItem>> GetGenresAsync();
+        IEnumerable<SelectListItem> GetGenres(IEnumerable<Movie> movies);
     }
 }
