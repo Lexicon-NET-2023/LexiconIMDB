477ae7a569305d28c7d2d337e861e88f98646e78
DZM77 <32109758+DZM77@users.noreply.github.com>
2023-11-01 13:19:08 +0100

    StarView Component

---
+++ LexiconIMDB/ViewComponents/StarViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using LexiconIMDB.Data;
+using LexiconIMDB.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LexiconIMDB.ViewComponents
+{
+    //[ViewComponent]
+    public class StarViewComponent : ViewComponent
+    {
+        //private readonly LexiconIMDBContext context;
+
+        //public StarViewComponent(LexiconIMDBContext context)
+        //{
+        //    this.context = context;
+        //}
+
+        public IViewComponentResult Invoke(float rating)
+        {
+            var doubleRating = (int)Math.Round(rating * 2);
+
+            var model = new StarViewModel
+            {
+                Stars = doubleRating / 2,
+                IsHalfStar = doubleRating % 2 == 1
+            };
+
+            return View(model);
+        }
+    }
+}
